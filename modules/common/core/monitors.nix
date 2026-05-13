b1995c01989a4fb53d86b20db660161e9f5058bb
iStellanova <istellanova@users.noreply.github.com>
2026-05-13 02:04:23 -0400

    Documentation for the hyprland address.

--- modules/common/core/monitors.nix
+++ modules/common/core/monitors.nix
@@ -2,8 +2,8 @@
   options.aspects.core.monitors = lib.mkOption {
     type = lib.types.attrsOf lib.types.str;
     default = {
-      DP-2 = "3440x1440@175, 1440x541, 1, sdrbrightness, 1.2, sdrsaturation, 0.98";
-      DP-3 = "2560x1440@100, 0x0, 1, transform, 1, sdrbrightness, 1.2, sdrsaturation, 0.98";
+      DP-2 = "3440x1440@175, 1440x541, 1";
+      DP-3 = "2560x1440@100, 0x0, 1, transform, 1";
     };
     description = "Centralized monitor configuration strings for graphical environments.";
   };
