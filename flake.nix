d65d6ac61eb95da40c2de2a3454c456d7257cc82
iStellanova <istellanova@users.noreply.github.com>
2026-05-20 09:06:53 -0400

    Removed the extra substitutes from the flake

--- flake.nix
+++ flake.nix
@@ -5,17 +5,6 @@
   # It leverages flake-parts for clean attribute separation and a custom recursive
   # module scanner in lib/ for automated feature discovery.
 
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-      "https://hyprland.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
