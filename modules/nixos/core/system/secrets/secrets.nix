1e9d230c90cae77417487621190aac20d25fe8f1
iStellanova <istellanova@users.noreply.github.com>
2026-05-21 04:43:58 -0400

    Added SSH key to secrets.

--- modules/nixos/core/system/secrets/secrets.nix
+++ modules/nixos/core/system/secrets/secrets.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  identity,
   ...
 }: {
   options.aspects.core.secrets.enable = lib.mkEnableOption "Secure secrets management using sops-nix";
@@ -16,6 +17,13 @@
     # Declare the user-password secret
     sops.secrets.user-password = {
       neededForUsers = true; # Critical: Decrypt before users are created!
+    };
+
+    # Decrypt and write the personal SSH private key dynamically on boot
+    sops.secrets.stellacode = {
+      path = "/home/${identity.name}/.ssh/stellacode";
+      owner = identity.name;
+      mode = "0600";
     };
   };
 }
