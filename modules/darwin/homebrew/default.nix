de2c82a1c5e310c1c69de77dd61458e5a724a24d
iStellanova <istellanova@users.noreply.github.com>
2026-05-22 03:15:51 -0400

    Created school dendrite.

--- modules/darwin/homebrew/default.nix
+++ modules/darwin/homebrew/default.nix
@@ -19,13 +19,10 @@
         "craft" # Craft - Notes Application
         "gimp" # GIMP - Image Editor
         "quicken" # Quicken
-        "webex" # WebEx
-        "zoom" # Zoom
       ];
 
       masApps = {
         "Dynamic wallpaper" = 1582358382; # Dynamic wallpaper - Wallpaper manager
-        "School Assistant" = 1465687472; # School Assistant - School management
         "Xcode" = 497799835; # Xcode - Apple IDE
       };
     };
