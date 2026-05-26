a726f5e8f7bc652a783df425caa767cf3e7d9dc1
stellanova <iStellanova@users.noreply.github.com>
2026-05-25 20:09:48 -0400

    Simplified meta.nix.

--- modules/meta.nix
+++ modules/meta.nix
@@ -1,4 +1,42 @@
-{lib, ...}: {
+{lib, ...}: let
+  mkIdentityOpts = homeDirDefault: {
+    username = lib.mkOption {
+      type = lib.types.singleLineStr;
+      default = "stellanova";
+      description = "The primary user username.";
+    };
+    homeDir = lib.mkOption {
+      type = lib.types.str;
+      default = homeDirDefault;
+      description = "The primary user home directory.";
+    };
+    userEmail = lib.mkOption {
+      type = lib.types.str;
+      default = "";
+      description = "The primary user email address.";
+    };
+    gitName = lib.mkOption {
+      type = lib.types.singleLineStr;
+      default = "stellanova";
+      description = "The name used in git commits.";
+    };
+    sshKeys = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [];
+      description = "Authorized SSH public keys.";
+    };
+    hashedPassword = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = "Hashed password for the user (when secrets aren't active).";
+    };
+    dataPath = lib.mkOption {
+      type = lib.types.nullOr lib.types.path;
+      default = null;
+      description = "Path to custom static assets (wallpapers, icons).";
+    };
+  };
+in {
   options = {
     hosts.nixos = lib.mkOption {
       type = lib.types.lazyAttrsOf (lib.types.submodule {
@@ -50,86 +88,7 @@
   };
 
   config = {
-    # NixOS level identity options
-    flake.modules.nixos.meta = {lib, ...}: {
-      options.identity = {
-        username = lib.mkOption {
-          type = lib.types.singleLineStr;
-          default = "stellanova";
-          description = "The primary user username.";
-        };
-        homeDir = lib.mkOption {
-          type = lib.types.str;
-          default = "/home/stellanova";
-          description = "The primary user home directory.";
-        };
-        userEmail = lib.mkOption {
-          type = lib.types.str;
-          default = "";
-          description = "The primary user email address.";
-        };
-        gitName = lib.mkOption {
-          type = lib.types.singleLineStr;
-          default = "stellanova";
-          description = "The name used in git commits.";
-        };
-        sshKeys = lib.mkOption {
-          type = lib.types.listOf lib.types.str;
-          default = [];
-          description = "Authorized SSH public keys.";
-        };
-        hashedPassword = lib.mkOption {
-          type = lib.types.nullOr lib.types.str;
-          default = null;
-          description = "Hashed password for the user (when secrets aren't active).";
-        };
-        dataPath = lib.mkOption {
-          type = lib.types.nullOr lib.types.path;
-          default = null;
-          description = "Path to custom static assets (wallpapers, icons).";
-        };
-      };
-    };
-
-    # Darwin level identity options
-    flake.modules.darwin.meta = {lib, ...}: {
-      options.identity = {
-        username = lib.mkOption {
-          type = lib.types.singleLineStr;
-          default = "stellanova";
-          description = "The primary user username.";
-        };
-        homeDir = lib.mkOption {
-          type = lib.types.str;
-          default = "/Users/stellanova";
-          description = "The primary user home directory.";
-        };
-        userEmail = lib.mkOption {
-          type = lib.types.str;
-          default = "";
-          description = "The primary user email address.";
-        };
-        gitName = lib.mkOption {
-          type = lib.types.singleLineStr;
-          default = "stellanova";
-          description = "The name used in git commits.";
-        };
-        sshKeys = lib.mkOption {
-          type = lib.types.listOf lib.types.str;
-          default = [];
-          description = "Authorized SSH public keys.";
-        };
-        hashedPassword = lib.mkOption {
-          type = lib.types.nullOr lib.types.str;
-          default = null;
-          description = "Hashed password for the user.";
-        };
-        dataPath = lib.mkOption {
-          type = lib.types.nullOr lib.types.path;
-          default = null;
-          description = "Path to custom static assets (wallpapers, icons).";
-        };
-      };
-    };
+    flake.modules.nixos.meta = _: {options.identity = mkIdentityOpts "/home/stellanova";};
+    flake.modules.darwin.meta = _: {options.identity = mkIdentityOpts "/Users/stellanova";};
   };
 }
