86b823cb11985d3fe686ce150d9bfa1932a5d257
stellanova <iStellanova@users.noreply.github.com>
2026-05-27 02:43:12 -0400

    Added plugins and nix-specific fixes for nautilus.

--- modules/programs/system/nautilus.nix
+++ modules/programs/system/nautilus.nix
@@ -1,16 +1,31 @@
 _: {
   # NixOS Nautilus Settings
-  flake.modules.nixos.nautilus = {pkgs, ...}: {
+  flake.modules.nixos.nautilus = {
+    pkgs,
+    lib,
+    ...
+  }: {
     config = {
       environment.systemPackages = with pkgs; [
         nautilus
         sushi
         evince
+        gst_all_1.gstreamer
         gst_all_1.gst-plugins-base
         gst_all_1.gst-plugins-good
         gst_all_1.gst-plugins-bad
         gst_all_1.gst-plugins-ugly
+        gst_all_1.gst-libav
       ];
+
+      environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (with pkgs.gst_all_1; [
+        gstreamer
+        gst-plugins-base
+        gst-plugins-good
+        gst-plugins-bad
+        gst-plugins-ugly
+        gst-libav
+      ]);
 
       services.gnome.tinysparql.enable = true;
       services.gnome.localsearch.enable = true;
