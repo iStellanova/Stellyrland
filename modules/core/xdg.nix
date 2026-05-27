1ad4d8a44b8969573f39b10fd8cf870907d3672e
stellanova <iStellanova@users.noreply.github.com>
2026-05-26 23:36:09 -0400

    Added defaults for stuff, something I forgot to do.

--- modules/core/xdg.nix
+++ modules/core/xdg.nix
@@ -31,18 +31,72 @@ _: {
 
     xdg.mimeApps = {
       enable = true;
-      defaultApplications = {
+      defaultApplications = let
+        browser = ["zen.desktop"];
+        editor = ["dev.zed.Zed.desktop"];
+        pdfViewer = ["org.gnome.Evince.desktop"];
+        fileManager = ["org.gnome.Nautilus.desktop"];
+        imageViewer = ["imv.desktop"];
+        musicPlayer = ["org.gnome.Lollypop.desktop"];
+        videoPlayer = ["mpv.desktop"];
+      in {
+        # Browser
+        "x-scheme-handler/http" = browser;
+        "x-scheme-handler/https" = browser;
+        "x-scheme-handler/chrome" = browser;
+        "text/html" = browser;
+        "application/x-extension-htm" = browser;
+        "application/x-extension-html" = browser;
+        "application/x-extension-shtml" = browser;
+        "application/xhtml+xml" = browser;
+        "application/x-extension-xhtml" = browser;
+        "application/x-extension-xht" = browser;
+
+        # URL schemes
         "x-scheme-handler/discord" = ["vesktop.desktop"];
-        "x-scheme-handler/http" = ["zen.desktop"];
-        "x-scheme-handler/https" = ["zen.desktop"];
-        "x-scheme-handler/chrome" = ["zen.desktop"];
-        "text/html" = ["zen.desktop"];
-        "application/x-extension-htm" = ["zen.desktop"];
-        "application/x-extension-html" = ["zen.desktop"];
-        "application/x-extension-shtml" = ["zen.desktop"];
-        "application/xhtml+xml" = ["zen.desktop"];
-        "application/x-extension-xhtml" = ["zen.desktop"];
-        "application/x-extension-xht" = ["zen.desktop"];
+
+        # File manager
+        "inode/directory" = fileManager;
+
+        # Text editor
+        "text/plain" = editor;
+        "text/markdown" = editor;
+        "text/x-script.python" = editor;
+        "application/json" = editor;
+        "application/x-shellscript" = editor;
+
+        # Image viewer
+        "image/jpeg" = imageViewer;
+        "image/png" = imageViewer;
+        "image/gif" = imageViewer;
+        "image/webp" = imageViewer;
+        "image/tiff" = imageViewer;
+        "image/bmp" = imageViewer;
+        "image/avif" = imageViewer;
+        "image/svg+xml" = imageViewer;
+
+        # Music player
+        "audio/mpeg" = musicPlayer;
+        "audio/flac" = musicPlayer;
+        "audio/ogg" = musicPlayer;
+        "audio/wav" = musicPlayer;
+        "audio/x-vorbis+ogg" = musicPlayer;
+        "audio/aac" = musicPlayer;
+        "audio/mp4" = musicPlayer;
+        "audio/x-flac" = musicPlayer;
+
+        # PDF viewer
+        "application/pdf" = pdfViewer;
+        "application/x-pdf" = pdfViewer;
+
+        # Video player
+        "video/mp4" = videoPlayer;
+        "video/x-matroska" = videoPlayer;
+        "video/webm" = videoPlayer;
+        "video/avi" = videoPlayer;
+        "video/quicktime" = videoPlayer;
+        "video/x-msvideo" = videoPlayer;
+        "video/mpeg" = videoPlayer;
       };
     };
   };
