564233fdded4ce5ee871fb1cf961a3f9cba20e37
stellanova <iStellanova@users.noreply.github.com>
2026-06-09 14:04:12 -0400

    feat(nix cache): Added cache for noctalia and zen.

--- modules/core/nix-settings.nix
+++ modules/core/nix-settings.nix
@@ -48,12 +48,16 @@ in {
             "https://hyprland.cachix.org"
             "https://nix-community.cachix.org"
             "https://attic.xuyh0120.win/lantian"
+            "https://zen-browser.cachix.org"
+            "https://noctalia.cachix.org"
           ];
           trusted-public-keys = [
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
             "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+            "zen-browser.cachix.org-1:z/QLGrEkiBYF/7zoHX1Hpuv0B26QrmbVBSy9yDD2tSs="
+            "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
           ];
         };
 
