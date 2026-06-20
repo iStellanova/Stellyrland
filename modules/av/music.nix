c82974a3eccb6e851733c22afd75530b99503b56
stellanova <iStellanova@users.noreply.github.com>
2026-06-20 06:20:40 -0400

    feat(music): Made kopuz nixos-only.

--- modules/av/music.nix
+++ modules/av/music.nix
@@ -7,13 +7,10 @@
 
   flake-file.inputs.kopuz.url = "github:Kopuz-org/kopuz";
 
-  sn.music.nixos = _: {
+  sn.music.nixos = {pkgs, ...}: {
     nix.settings.substituters = ["https://kopuz.cachix.org"];
     nix.settings.trusted-public-keys = ["kopuz.cachix.org-1:J2X3AnAYhKTJW5S3aCLoA1ckonQXVNZMQvhZA0YAufw="];
-  };
-
-  sn.music.homeManager = {pkgs, ...}: {
-    home.packages = [
+    environment.systemPackages = [
       inputs.kopuz.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
   };
