26bd76b25a47d3f27093787037a860fe89359cfb
stellanova <iStellanova@users.noreply.github.com>
2026-06-26 02:10:10 -0400

    feat(hyprland): Converted raw lua to HM configurations.

--- modules/hosts/stellyrland/aspect.nix
+++ modules/hosts/stellyrland/aspect.nix
@@ -46,11 +46,43 @@
       desktop.noctalia.primaryMonitor = "DP-2";
       desktop.noctalia.secondaryMonitor = "DP-3";
 
-      desktop.hyprland.monitorConfig = ''
-        hl.monitor({ output = "DP-2", mode = "3440x1440@175", position = "1440x541", scale = 1, bitdepth = 10, cm = "hdr", supports_wide_color = 1, sdr_min_luminance = 0.0,  sdr_max_luminance = 203, sdrbrightness = 0.75, sdrsaturation = 1.2, min_luminance = 0.0005, max_luminance = 1000, max_avg_luminance = 250 })
-        hl.monitor({ output = "DP-3", mode = "2560x1440@100", position = "0x0",    scale = 1, transform = 1, bitdepth = 10, cm = "srgb", sdr_min_luminance = 0.2, min_luminance = 0.25, max_luminance = 250, max_avg_luminance = 250 })
-        hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1 })
-      '';
+      desktop.hyprland.monitors = [
+        {
+          output = "DP-2";
+          mode = "3440x1440@175";
+          position = "1440x541";
+          scale = 1;
+          bitdepth = 10;
+          cm = "hdr";
+          supports_wide_color = 1;
+          sdr_min_luminance = 0.0;
+          sdr_max_luminance = 203;
+          sdrbrightness = 0.75;
+          sdrsaturation = 1.2;
+          min_luminance = 0.0005;
+          max_luminance = 1000;
+          max_avg_luminance = 250;
+        }
+        {
+          output = "DP-3";
+          mode = "2560x1440@100";
+          position = "0x0";
+          scale = 1;
+          transform = 1;
+          bitdepth = 10;
+          cm = "srgb";
+          sdr_min_luminance = 0.2;
+          min_luminance = 0.25;
+          max_luminance = 250;
+          max_avg_luminance = 250;
+        }
+        {
+          output = "";
+          mode = "preferred";
+          position = "auto";
+          scale = 1;
+        }
+      ];
     };
   };
 }
