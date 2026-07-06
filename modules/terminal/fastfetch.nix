70419cdeb96e6eae57d53877943d47c059c8863d
stellanova <iStellanova@users.noreply.github.com>
2026-07-06 15:50:46 -0400

    fix(fastfetch): Cleans the config, removed the schema and empty logo.

--- modules/terminal/fastfetch.nix
+++ modules/terminal/fastfetch.nix
@@ -19,8 +19,6 @@
       programs.fastfetch = {
         enable = true;
         settings = {
-          "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
-          logo = { };
           display = {
             separator = " ";
           };
