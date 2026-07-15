3db2118d82b9a7e4ef259a4c1992c58af2eed295
stellanova <iStellanova@users.noreply.github.com>
2026-07-15 16:35:04 -0400

    feat(run0): Replaced sudo with run0.

--- modules/nix-base/core.nix
+++ modules/nix-base/core.nix
@@ -1,23 +1,41 @@
 _: {
-  flake.modules.nixos.core = _: {
-    time.timeZone = "America/Indianapolis";
-    i18n.defaultLocale = "en_US.UTF-8";
+  flake.modules.nixos.core =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      time.timeZone = "America/Indianapolis";
+      i18n.defaultLocale = "en_US.UTF-8";
 
-    security.sudo-rs = {
-      enable = true;
-      extraConfig = "Defaults pwfeedback";
-    };
+      security.sudo.enable = false;
+      security.sudo-rs.enable = false;
+      security.run0 = {
+        enable = true;
+        sudo-shim.enable = true;
+      };
 
-    system.stateVersion = "25.11";
-    documentation.nixos.enable = false;
-    programs.ssh.startAgent = true;
-    services.gnome.gcr-ssh-agent.enable = false;
-    systemd.oomd.enable = true;
-    systemd.services."systemd-udevd".serviceConfig = {
-      TimeoutStartSec = "10s";
-      TimeoutStopSec = "10s";
+      # Disable the background.
+      environment.systemPackages = [
+        (lib.hiPrio (
+          pkgs.writeShellScriptBin "run0" ''
+            exec ${lib.getExe' config.systemd.package "run0"} --background= "$@"
+          ''
+        ))
+      ];
+
+      system.stateVersion = "25.11";
+      documentation.nixos.enable = false;
+      programs.ssh.startAgent = true;
+      services.gnome.gcr-ssh-agent.enable = false;
+      systemd.oomd.enable = true;
+      systemd.services."systemd-udevd".serviceConfig = {
+        TimeoutStartSec = "10s";
+        TimeoutStopSec = "10s";
+      };
     };
-  };
 
   flake.modules.homeManager.core = _: {
     home.sessionPath = [ "$HOME/.local/state/nix/profiles/scratch/bin" ];
