bdd4bc6546874e7f2be90fdf4b97cdbaec145029
stellanova <iStellanova@users.noreply.github.com>
2026-07-16 13:10:08 -0400

    feat(greeter): Added new hm options to greeter.

--- modules/desktop/noctalia-greeter.nix
+++ modules/desktop/noctalia-greeter.nix
@@ -5,24 +5,38 @@
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  flake.modules.nixos.noctalia-greeter = { ... }: {
-    imports = [ inputs.noctalia-greeter.nixosModules.default ];
+  flake.modules.nixos.noctalia-greeter =
+    {
+      host,
+      lib,
+      pkgs,
+      ...
+    }:
+    {
+      imports = [ inputs.noctalia-greeter.nixosModules.default ];
 
-    programs.noctalia-greeter = {
-      enable = true;
-      greeter-args = "--session hyprland";
-      settings = {
-        cursor = {
-          theme = "Bibata-Modern-Ice";
-          size = 16;
+      programs.noctalia-greeter = {
+        enable = true;
+        settings = {
+          session.default = "hyprland";
+          user.default = "stellanova";
+          output = {
+            name = lib.elemAt host.monitorPriority 0;
+            width = 3440;
+            height = 1440;
+          };
+          cursor = {
+            theme = "Bibata-Modern-Ice";
+            size = 16;
+            path = "${pkgs.bibata-cursors}/share/icons";
+          };
+          keyboard.layout = "us";
+          appearance.password_style = "random";
         };
-        keyboard.layout = "us";
-        appearance.password_style = "random";
       };
-    };
 
-    systemd.tmpfiles.rules = [
-      "d /persist/var/lib/noctalia-greeter 0750 greeter greeter -"
-    ];
-  };
+      systemd.tmpfiles.rules = [
+        "d /persist/var/lib/noctalia-greeter 0750 greeter greeter -"
+      ];
+    };
 }
