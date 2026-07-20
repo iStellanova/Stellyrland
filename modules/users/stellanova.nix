fece8149ada14cf3016d0ee113b2e03d0367aecc
stellanova <iStellanova@users.noreply.github.com>
2026-07-20 01:48:10 -0400

    refactor(factory): fold home-manager wiring into factory.user
    The home-manager module import and per-user homeManager defaults
    (home.username, home.homeDirectory, programs.zsh.enable) were hand-
    repeated in every users/<name>.nix. Moving them into factory.user lets
    each user file shrink to a single factory call.

--- modules/users/stellanova.nix
+++ modules/users/stellanova.nix
@@ -1,30 +1,3 @@
-{
-  self,
-  lib,
-  ...
-}:
-{
-  flake.modules = lib.mkMerge [
-    (self.factory.user "stellanova" true)
-    {
-      nixos.stellanova = {
-        imports = [
-          self.modules.nixos.home-manager
-        ];
-      };
-
-      darwin.stellanova = {
-        imports = [
-          self.modules.darwin.home-manager
-        ];
-      };
-
-      homeManager.stellanova =
-        { host, ... }:
-        {
-          home.homeDirectory = host.homeDir;
-          programs.zsh.enable = true;
-        };
-    }
-  ];
+{ self, ... }: {
+  flake.modules = self.factory.user "stellanova" true;
 }
