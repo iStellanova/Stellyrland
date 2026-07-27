0d16455bb9f53d884dce0ac0ae46eadbf3035262
stellanova <iStellanova@users.noreply.github.com>
2026-07-27 00:50:50 -0400

    fix(nixfmt): Skip the encrypted files.

--- modules/treefmt.nix
+++ modules/treefmt.nix
@@ -9,7 +9,8 @@
   perSystem = {
     treefmt = {
       projectRootFile = "flake.nix";
-      settings.global.excludes = [ ".tack/*" ];
+      # git-crypt encrypted; appears as ciphertext when the key isn't unlocked locally
+      settings.global.excludes = [ ".tack/*" "modules/hosts/famtop/_identity.nix" ];
       programs = {
         nixfmt.enable = true;
         deadnix.enable = true;
