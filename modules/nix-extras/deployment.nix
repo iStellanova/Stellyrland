36adf979d09bce8a446f632b6f292f5f60436cb9
stellanova <iStellanova@users.noreply.github.com>
2026-08-13 07:40:59 -0400

    feat(deployment): Simplified deployment, removed darwin hosts. No point.

--- modules/nix-extras/deployment.nix
+++ modules/nix-extras/deployment.nix
@@ -2,32 +2,21 @@ _:
 let
   targets = {
     ItsRedFlame = {
-      kind = "nixos";
-      hostName = "itsredflame.tailb15b96.ts.net";
       hostNames = [ "itsredflame.tailb15b96.ts.net" ];
       publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJgWFxH/qllaNX5axfrDfplGpn/URESTGNX/t4TGgJ6q";
     };
     plasmapulsefinale = {
-      kind = "nixos";
-      hostName = "plasmapulsefinale.tailb15b96.ts.net";
       hostNames = [ "plasmapulsefinale.tailb15b96.ts.net" ];
       publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB+IEKcruyMNZYEp9PHYHC6Z/AkEoJnxByunqql9zO71";
     };
     stellyrland = {
-      kind = "nixos";
-      hostName = "stellyrland.tailb15b96.ts.net";
       hostNames = [
         "stellyrland.local"
         "stellyrland.tailb15b96.ts.net"
       ];
       publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAPDq0bTLCKn1lKqYn+22wRYiEsNFoMvMlRh1Klm8edA";
     };
-    stellyrtop = {
-      kind = "darwin";
-      hostName = "stellyrtop.tailb15b96.ts.net";
-      hostNames = [ "stellyrtop.tailb15b96.ts.net" ];
-      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJuUvoUL9cDXQKdtmHxidbuOK8iuC/ItVWyPFzXySxSm";
-    };
+
   };
 in
 {
@@ -52,7 +41,7 @@ in
         distributedBuilds = true;
         buildMachines = [
           {
-            inherit (targets.stellyrland) hostName;
+            hostName = builtins.head targets.stellyrland.hostNames;
             system = "x86_64-linux";
             protocol = "ssh-ng";
             sshUser = host.username;
@@ -76,89 +65,49 @@ in
           )
         )
       );
-      deploymentAliases = lib.listToAttrs (
+      deploymentSSHAliases = lib.listToAttrs (
         lib.mapAttrsToList (name: target: {
           name = "deploy-${name}";
           value = {
-            HostName = target.hostName;
+            HostName = builtins.head target.hostNames;
             User = "stellanova";
             IdentityFile = "/run/secrets/stellyrlab-deploy-key";
             IdentitiesOnly = "yes";
             BatchMode = "yes";
-            SetEnv.TERM = "xterm-256color";
             StrictHostKeyChecking = "yes";
             UserKnownHostsFile = toString deploymentKnownHosts;
           };
         }) targets
-      );
-      deploymentTypes = lib.concatStringsSep "\n" (
-        lib.mapAttrsToList (name: target: "  ${name} ${target.kind}") targets
       );
     in
     {
       programs.ssh = {
         enable = true;
         enableDefaultConfig = false;
-        settings = deploymentAliases // {
-          stellyrtop = {
-            SetEnv.TERM = "xterm-256color";
-          };
-        };
+        settings = deploymentSSHAliases;
       };
-
       programs.zsh.initContent = ''
-                _deployment_prep() {
-                  (cd "$FLAKE" && nix fmt -- --ci) && git -C "$FLAKE" --no-pager diff --check
-                }
+        deploy() {
+          if [[ -z "$1" ]]; then
+            echo "Usage: deploy <host> [check]"
+            return 1
+          fi
 
-                typeset -A _deployment_types=(
-        ${deploymentTypes}
-                )
-
-                deploy() {
-                  if [[ -z "$1" ]]; then
-                    echo "Usage: deploy <host> [check|extra-args...]"
-                    return 1
-                  fi
-                  local target="$1"
-                  shift
-
-                  if [[ "$target" == stellyrlab ]]; then
-                    if [[ "$1" == "check" ]]; then
-                      shift
-                      _deployment_prep && nixos-rebuild build --flake "$FLAKE#$target" "$@"
-                    else
-                      _deployment_prep && nixos-rebuild switch --flake "$FLAKE#$target" --elevate=run0 "$@"
-                    fi
-                    return $?
-                  fi
-
-                  local deployment_type="''${_deployment_types[$target]}"
-                  case "$deployment_type" in
-                    nixos)
-                      local target_host=(--target-host "deploy-$target")
-                      if [[ "$1" == "check" ]]; then
-                        shift
-                        _deployment_prep && nh os switch "$FLAKE#$target" --hostname "$target" "''${target_host[@]}" --elevation-strategy=program:sudo --dry --diff always "$@"
-                      else
-                        _deployment_prep && nh os switch "$FLAKE#$target" --hostname "$target" "''${target_host[@]}" --elevation-strategy=program:sudo --diff always "$@"
-                      fi
-                      ;;
-                    darwin)
-                      local source_flake="github:iStellanova/Stellyrland/$(git -C "$FLAKE" rev-parse HEAD)"
-                      if [[ "$1" == "check" ]]; then
-                        shift
-                        _deployment_prep && ssh "deploy-$target" "nh darwin build '$source_flake#$target' --hostname '$target' --diff always"
-                      else
-                        _deployment_prep && ssh -tt "deploy-$target" "nh darwin switch '$source_flake#$target' --hostname '$target' --elevation-strategy=program:sudo --diff always"
-                      fi
-                      ;;
-                    *)
-                      echo "No controller deployment path for $target"
-                      return 1
-                      ;;
-                  esac
-                }
+          local target="$1"
+          local check="''${2:-}"
+          local dry=""
+          if [[ -n "$check" && "$check" != check ]]; then
+            echo "Usage: deploy <host> [check]"
+            return 1
+          fi
+          [[ "$check" == check ]] && dry="--dry"
+          local remote="deploy-$target"
+          if [[ "$target" == "$(hostname)" ]]; then
+            nh os switch "$FLAKE#$target" --hostname "$target" --elevation-strategy=run0 $dry --diff always
+          else
+            nh os switch "$FLAKE#$target" --hostname "$target" --target-host "$remote" --elevation-strategy=program:sudo $dry --diff always
+          fi
+        }
       '';
     };
 }
