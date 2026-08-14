f158335729b574d2d09fbf6091e25d379b23b234
stellanova <iStellanova@users.noreply.github.com>
2026-08-14 18:30:13 -0400

    audit(nix-tools): Cleaned up nix-tools. Removed tons of aliases.

--- modules/nix/nix-tools.nix
+++ modules/nix/nix-tools.nix
@@ -3,17 +3,13 @@ _: {
   flake.modules.homeManager.nix-tools =
     {
       host,
-      pkgs,
       ...
     }:
     {
       programs.zsh.shellAliases = {
-        clean = "nh clean all --keep 20";
+        clean = "nh clean all --keep 20 --optimise";
         cdn = "cd $FLAKE";
-        nixinfo = "nh os info";
         replace = "(cd $FLAKE && nix run .#write-tack)";
-        nix-list = "nix profile list --profile ~/.local/state/nix/profiles/scratch";
-        nix-clear = "rm -rf ~/.local/state/nix/profiles/scratch && nh clean all --keep 20";
       };
 
       programs.zsh.initContent = ''
@@ -22,66 +18,6 @@ _: {
         elif [[ -s "$HOME/.config/github-token" ]]; then
           export GITHUB_TOKEN="$(cat "$HOME/.config/github-token")"
         fi
-
-        _nix_prep() {
-          (cd "$FLAKE" && nix fmt -- --ci) && git -C "$FLAKE" --no-pager diff --check
-        }
-
-        rebuild() {
-          if [[ "$1" == "check" ]]; then
-            _nix_prep && ${
-              if pkgs.stdenv.hostPlatform.isDarwin then
-                "nh darwin build $FLAKE && rm -f ./result"
-              else
-                "nh os build $FLAKE --diff always && rm -f ./result"
-            }
-          else
-            _nix_prep && ${
-              if pkgs.stdenv.hostPlatform.isDarwin then "nh darwin switch $FLAKE" else "nh os switch $FLAKE"
-            }
-          fi
-        }
-
-        upgrade() {
-          if [[ "$1" == "check" ]]; then
-            (cd "$FLAKE" && nix develop . --accept-flake-config --command tack look --verbose) && _nix_prep && ${
-              if pkgs.stdenv.hostPlatform.isDarwin then
-                "nh darwin build $FLAKE && rm -f ./result"
-              else
-                "nh os build $FLAKE --diff always && rm -f ./result"
-            }
-          else
-            (cd "$FLAKE" && nix run .#write-tack) && _nix_prep && ${
-              if pkgs.stdenv.hostPlatform.isDarwin then "nh darwin switch $FLAKE" else "nh os switch $FLAKE"
-            }
-          fi
-        }
-
-        notify_remote() {
-          if [[ -z "$1" ]]; then
-            echo "Usage: notify_remote <host> [title] [message]"
-            return 1
-          fi
-          local target="$1"
-          local title="''${2:-System Update}"
-          local msg="''${3:-Done! Deployment finished successfully.}"
-          ssh -n -o ConnectTimeout=4 "stellanova@$target" \
-            "BUS=\$(sudo find /run/user -maxdepth 2 -name bus 2>/dev/null | grep -v \"/run/user/\$(id -u)/\" | head -n 1); \
-             if [[ -z \"\$BUS\" ]]; then \
-               BUS=\$(sudo find /run/user -maxdepth 2 -name bus 2>/dev/null | head -n 1); \
-             fi; \
-             if [[ -n \"\$BUS\" ]]; then \
-               UID_VAL=\$(echo \"\$BUS\" | cut -d'/' -f4); \
-               sudo systemd-run --quiet --uid=\"\$UID_VAL\" --setenv=DBUS_SESSION_BUS_ADDRESS=\"unix:path=\$BUS\" notify-send -i system-software-update \"$title\" \"$msg\" 2>/dev/null || true; \
-             fi" 2>/dev/null || true
-        }
-
-
-        nix-add() { local profile="$HOME/.local/state/nix/profiles/scratch"; NIXPKGS_ALLOW_UNFREE=1 nix profile add --profile "$profile" --impure nixpkgs#$1; }
-        nix-remove() {
-          if [[ ! -d ~/.local/state/nix/profiles/scratch ]]; then echo "Scratch profile doesn't exist"; return 1; fi
-          nix profile remove --profile ~/.local/state/nix/profiles/scratch $1
-        }
       '';
 
       programs.nh = {
