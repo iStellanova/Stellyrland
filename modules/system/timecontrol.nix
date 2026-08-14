b1c468d73c069ed724f931d1b00b523f50a9c6ed
stellanova <iStellanova@users.noreply.github.com>
2026-08-14 06:11:15 -0400

    audit(timecontrol): Ran through an audit with ponytail.

--- modules/system/timecontrol.nix
+++ modules/system/timecontrol.nix
@@ -47,90 +47,90 @@ _: {
         };
       };
 
-      config = lib.mkIf config.timecontrol.enable {
-        assertions = [
-          {
-            assertion = lib.all (day: builtins.elem day weekdays) (
-              builtins.attrNames config.timecontrol.schedule
-            );
-            message = "timecontrol.schedule keys must be weekday names.";
-          }
-          {
-            assertion = lib.all (window: window.start < window.end) (
-              lib.concatLists (lib.attrValues config.timecontrol.schedule)
-            );
-            message = "timecontrol windows must start before they end; overnight windows are unsupported.";
-          }
-        ];
+      config = lib.mkIf config.timecontrol.enable (
+        let
+          windows = lib.concatLists (
+            lib.mapAttrsToList (
+              day: dayWindows:
+              lib.imap0 (index: window: { inherit day index window; }) dayWindows
+            ) config.timecontrol.schedule
+          );
+        in
+        {
+          assertions = [
+            {
+              assertion = lib.all (day: builtins.elem day weekdays) (
+                builtins.attrNames config.timecontrol.schedule
+              );
+              message = "timecontrol.schedule keys must be weekday names.";
+            }
+            {
+              assertion = lib.all (window: window.start < window.end) (
+                lib.concatLists (lib.attrValues config.timecontrol.schedule)
+              );
+              message = "timecontrol windows must start before they end; overnight windows are unsupported.";
+            }
+          ];
 
-        systemd.services = lib.listToAttrs (
-          lib.concatLists (
-            lib.mapAttrsToList (
-              day: windows:
-              lib.imap0 (index: _window: {
-                name = "timecontrol-start-${lib.toLower day}-${toString index}";
-                value = {
-                  description = "Allow ${config.timecontrol.user} logins on ${day}";
-                  serviceConfig = {
-                    Type = "oneshot";
-                    ExecStart = "${pkgs.shadow}/bin/passwd -u ${lib.escapeShellArg config.timecontrol.user}";
+          systemd.services = lib.listToAttrs (
+            lib.concatMap (
+              { day, index, ... }:
+              [
+                {
+                  name = "timecontrol-start-${lib.toLower day}-${toString index}";
+                  value = {
+                    description = "Allow ${config.timecontrol.user} logins on ${day}";
+                    serviceConfig = {
+                      Type = "oneshot";
+                      ExecStart = "${pkgs.shadow}/bin/passwd -u ${lib.escapeShellArg config.timecontrol.user}";
+                    };
                   };
-                };
-              }) windows
-            ) config.timecontrol.schedule
-          )
-          ++ lib.concatLists (
-            lib.mapAttrsToList (
-              day: windows:
-              lib.imap0 (index: _window: {
-                name = "timecontrol-stop-${lib.toLower day}-${toString index}";
-                value = {
-                  description = "End ${config.timecontrol.user} login time on ${day}";
-                  serviceConfig = {
-                    Type = "oneshot";
-                    ExecStart = pkgs.writeShellScript "timecontrol-stop-${lib.toLower day}-${toString index}" ''
-                      ${pkgs.systemd}/bin/loginctl terminate-user ${lib.escapeShellArg config.timecontrol.user} || true
-                      exec ${pkgs.shadow}/bin/passwd -l ${lib.escapeShellArg config.timecontrol.user}
-                    '';
+                }
+                {
+                  name = "timecontrol-stop-${lib.toLower day}-${toString index}";
+                  value = {
+                    description = "End ${config.timecontrol.user} login time on ${day}";
+                    serviceConfig = {
+                      Type = "oneshot";
+                      ExecStart = pkgs.writeShellScript "timecontrol-stop-${lib.toLower day}-${toString index}" ''
+                        ${pkgs.systemd}/bin/loginctl terminate-user ${lib.escapeShellArg config.timecontrol.user} || true
+                        exec ${pkgs.shadow}/bin/passwd -l ${lib.escapeShellArg config.timecontrol.user}
+                      '';
+                    };
                   };
-                };
-              }) windows
-            ) config.timecontrol.schedule
-          )
-        );
+                }
+              ]
+            ) windows
+          );
 
-        systemd.timers = lib.listToAttrs (
-          lib.concatLists (
-            lib.mapAttrsToList (
-              day: windows:
-              lib.imap0 (index: window: {
-                name = "timecontrol-start-${lib.toLower day}-${toString index}";
-                value = {
-                  wantedBy = [ "timers.target" ];
-                  timerConfig = {
-                    OnCalendar = "${day} ${window.start}";
-                    Persistent = true;
+          systemd.timers = lib.listToAttrs (
+            lib.concatMap (
+              { day, index, window }:
+              [
+                {
+                  name = "timecontrol-start-${lib.toLower day}-${toString index}";
+                  value = {
+                    wantedBy = [ "timers.target" ];
+                    timerConfig = {
+                      OnCalendar = "${day} ${window.start}";
+                      Persistent = true;
+                    };
                   };
-                };
-              }) windows
-            ) config.timecontrol.schedule
-          )
-          ++ lib.concatLists (
-            lib.mapAttrsToList (
-              day: windows:
-              lib.imap0 (index: window: {
-                name = "timecontrol-stop-${lib.toLower day}-${toString index}";
-                value = {
-                  wantedBy = [ "timers.target" ];
-                  timerConfig = {
-                    OnCalendar = "${day} ${window.end}";
-                    Persistent = true;
+                }
+                {
+                  name = "timecontrol-stop-${lib.toLower day}-${toString index}";
+                  value = {
+                    wantedBy = [ "timers.target" ];
+                    timerConfig = {
+                      OnCalendar = "${day} ${window.end}";
+                      Persistent = true;
+                    };
                   };
-                };
-              }) windows
-            ) config.timecontrol.schedule
-          )
-        );
-      };
+                }
+              ]
+            ) windows
+          );
+        }
+      );
     };
 }
