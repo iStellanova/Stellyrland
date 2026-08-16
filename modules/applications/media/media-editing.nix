c31373b94eff93e712f75075be7e075acd5f064f
stellanova <iStellanova@users.noreply.github.com>
2026-08-15 23:44:50 -0400

    audit(media-editing): Brought losslesscut back

--- modules/applications/media/media-editing.nix
+++ modules/applications/media/media-editing.nix
@@ -1,12 +1,14 @@
 {
   flake.modules.nixos.media-editing = { pkgs, ... }: {
     environment.systemPackages = with pkgs; [
+      losslesscut-bin
       gimp
       parabolic
     ];
   };
 
-  flake.modules.darwin.media-editing = {
+  flake.modules.darwin.media-editing = { pkgs, ... }: {
+    environment.systemPackages = [ pkgs.losslesscut-bin ];
     homebrew.casks = [
       "gimp"
     ];
