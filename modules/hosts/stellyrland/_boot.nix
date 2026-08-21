5563e7fd7d424b576f61c1db9c885b4215b5ea3e
stellanova <iStellanova@users.noreply.github.com>
2026-08-21 16:56:44 -0400

    fix(cache): Moved kernel cache to global.

--- modules/hosts/stellyrland/_boot.nix
+++ modules/hosts/stellyrland/_boot.nix
@@ -93,9 +93,6 @@
   };
 
   nixpkgs.overlays = [ inputs.cachyos-kernel.overlays.pinned ];
-  nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
-  nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
-
   boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore-lto-x86_64-v4;
   boot.zfs.package = config.boot.kernelPackages.zfs_cachyos;
 
