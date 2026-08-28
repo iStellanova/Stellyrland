c3085c4fff6758fef3cd8c62c7091d6462b4a51f
stellanova <iStellanova@users.noreply.github.com>
2026-08-28 01:16:43 -0400

    Update default.nix

--- modules/hosts/stellyrlab/default.nix
+++ modules/hosts/stellyrlab/default.nix
@@ -14,38 +14,44 @@
 
       networking = {
         hostName = host.name;
-        networkmanager.enable = true;
-        networkmanager.ensureProfiles.profiles.stellyrland-direct = {
-          connection = {
-            id = "stellyrland-direct";
-            type = "ethernet";
-            interface-name = "eno2";
+        networkmanager = {
+          enable = true;
+          ensureProfiles.profiles.stellyrland-direct = {
+            connection = {
+              id = "stellyrland-direct";
+              type = "ethernet";
+              interface-name = "eno2";
+            };
+            ipv4 = {
+              method = "manual";
+              addresses = "172.31.255.1/30";
+            };
+            ipv6.method = "disabled";
           };
-          ipv4 = {
-            method = "manual";
-            addresses = "172.31.255.1/30";
-          };
-          ipv6.method = "disabled";
         };
       };
-      nix.settings.trusted-users = [ host.username ];
-      nix.settings.max-jobs = 2;
-      nix.settings.cores = 4;
+      nix.settings = {
+        trusted-users = [ host.username ];
+        max-jobs = 2;
+        cores = 4;
+      };
       users.users.${host.username}.linger = true;
       system.stateVersion = "26.05";
 
       boot = {
         kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore-lto-x86_64-v3;
-        zfs.package = config.boot.kernelPackages.zfs_cachyos;
-        loader.systemd-boot = {
-          enable = true;
-          configurationLimit = 15;
-          consoleMode = "max";
-        };
-        loader.efi.canTouchEfiVariables = true;
         zfs = {
+          package = config.boot.kernelPackages.zfs_cachyos;
           devNodes = "/dev/mapper";
           forceImportRoot = true;
+        };
+        loader = {
+          systemd-boot = {
+            enable = true;
+            configurationLimit = 15;
+            consoleMode = "max";
+          };
+          efi.canTouchEfiVariables = true;
         };
         initrd = {
           supportedFilesystems = [ "zfs" ];
@@ -61,9 +67,34 @@
         };
       };
 
-      services.logind.settings.Login = {
-        HandleLidSwitch = "ignore";
-        HandleLidSwitchExternalPower = "ignore";
+      services = {
+        logind.settings.Login = {
+          HandleLidSwitch = "ignore";
+          HandleLidSwitchExternalPower = "ignore";
+        };
+
+        # Selective snapshots support local rollback and HDD replication.
+        sanoid = {
+          enable = true;
+          datasets = {
+            "zroot/safe/home".useTemplate = [ "default" ];
+            "zroot/safe/srv".useTemplate = [ "default" ];
+          };
+          templates.default = {
+            hourly = 0;
+            daily = 7;
+            weekly = 0;
+            monthly = 0;
+            yearly = 0;
+            autosnap = true;
+            autoprune = true;
+          };
+        };
+        zfs.autoScrub = {
+          enable = true;
+          interval = "monthly";
+          pools = [ "zroot" ];
+        };
       };
 
       hardware.enableRedistributableFirmware = true;
@@ -73,27 +104,5 @@
         wakeonlan
       ];
 
-      # Selective snapshots support local rollback and HDD replication.
-      services.sanoid = {
-        enable = true;
-        datasets = {
-          "zroot/safe/home".useTemplate = [ "default" ];
-          "zroot/safe/srv".useTemplate = [ "default" ];
-        };
-        templates.default = {
-          hourly = 0;
-          daily = 7;
-          weekly = 0;
-          monthly = 0;
-          yearly = 0;
-          autosnap = true;
-          autoprune = true;
-        };
-      };
-      services.zfs.autoScrub = {
-        enable = true;
-        interval = "monthly";
-        pools = [ "zroot" ];
-      };
     };
 }
