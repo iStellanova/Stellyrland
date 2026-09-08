5da1fdb7c8cdb0a02e0c177462cec9e2aa0e92f3
stellanova <iStellanova@users.noreply.github.com>
2026-09-08 11:04:34 -0400

    fix(pycharm): Patched until cythonDebugSpeedupsHook support comes around.

--- modules/applications/development/ide-suite.nix
+++ modules/applications/development/ide-suite.nix
@@ -1,10 +1,21 @@
 let
-  osShared = { pkgs, ... }: {
-    environment.systemPackages = with pkgs; [
-      jetbrains.clion
-      jetbrains.pycharm
-    ];
-  };
+  osShared = { pkgs, ... }:
+    let
+      pycharm =
+        if pkgs.stdenv.hostPlatform.isDarwin then
+          pkgs.jetbrains.pycharm.overrideAttrs (old: {
+            # TODO(nixpkgs): Remove when cythonDebugSpeedupsHook supports PyCharm 2026.2's python-ce layout.
+            nativeBuildInputs = pkgs.lib.remove pkgs.jetbrains.cythonDebugSpeedupsHook old.nativeBuildInputs;
+          })
+        else
+          pkgs.jetbrains.pycharm;
+    in
+    {
+      environment.systemPackages = with pkgs; [
+        jetbrains.clion
+        pycharm
+      ];
+    };
 in
 {
   flake.modules.nixos.ide-suite = { pkgs, ... }: {
