c43c870161366688a89abd39bb2d7a9722e8b32b
stellanova <iStellanova@users.noreply.github.com>
2026-09-13 23:55:24 -0400

    fix(pycharm): Fixed pycharm build on darwin.

--- modules/applications/development/ide-suite.nix
+++ modules/applications/development/ide-suite.nix
@@ -1,10 +1,16 @@
 let
   osShared =
-    { pkgs, ... }:
+    { pkgs, lib, ... }:
     {
       environment.systemPackages = with pkgs; [
         jetbrains.clion
-        pkgs.jetbrains.pycharm
+        (if stdenv.hostPlatform.isDarwin then
+          # TODO(pycharm): remove when the Darwin package restores its Cython helper path.
+          jetbrains.pycharm.overrideAttrs (old: {
+            nativeBuildInputs = lib.remove jetbrains.cythonDebugSpeedupsHook old.nativeBuildInputs;
+          })
+        else
+          jetbrains.pycharm)
       ];
     };
 in
